7d1c140811dfabc389a8ea0db0361a1cf3f6a86e
Artiom Tofan <artiomtofancv@gmail.com>
2020-05-07 10:09:47 +0200

    testing dist

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 4200);
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/dist/WARM/index.html'));
+});
+
+console.log('console is listening');
